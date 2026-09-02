d8a1bc1ad9266f7f5847808051cf69e2b46a9d64
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-09-02 17:04:00 +0300

    android: add application composition root

---
+++ android/app/src/main/java/com/smarttraffic/app/SmartTrafficApp.kt
@@ -0,0 +1,9 @@
+package com.smarttraffic.app
+
+import androidx.compose.runtime.Composable
+import com.smarttraffic.app.navigation.AppNavHost
+
+@Composable
+fun SmartTrafficApp() {
+    AppNavHost()
+}
