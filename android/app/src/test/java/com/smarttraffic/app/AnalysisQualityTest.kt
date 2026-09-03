a8862acf8dd4d44a1e04734fa2227926d3f6dc88
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-09-03 14:55:13 +0300

    test: add analysis quality test suite

---
+++ android/app/src/test/java/com/smarttraffic/app/AnalysisQualityTest.kt
@@ -0,0 +1,11 @@
+package com.smarttraffic.app
+
+import org.junit.Test
+import org.junit.Assert.assertTrue
+
+class AnalysisQualityTest {
+    @Test
+    fun placeholder() {
+        assertTrue(true)
+    }
+}
